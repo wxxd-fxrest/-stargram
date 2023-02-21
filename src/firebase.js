e39502ab5f98549b7c90fe3dda705c0c2203b7e5
wxxd-fxrest <fxrest704@gmail.com>
2023-02-22 01:20:59 +0900

    2/21 - firebase key edit

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,19 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDlcr4dEzrhYFoOkR7oML_bicHp0vEeDZ4",
-    authDomain: "wood-forest-stargram.firebaseapp.com",
-    projectId: "wood-forest-stargram",
-    storageBucket: "wood-forest-stargram.appspot.com",
-    messagingSenderId: "586124997156",
-    appId: "1:586124997156:web:f4c15da2f5353f8d229eb1"
+    // apiKey: "AIzaSyDlcr4dEzrhYFoOkR7oML_bicHp0vEeDZ4",
+    // authDomain: "wood-forest-stargram.firebaseapp.com",
+    // projectId: "wood-forest-stargram",
+    // storageBucket: "wood-forest-stargram.appspot.com",
+    // messagingSenderId: "586124997156",
+    // appId: "1:586124997156:web:f4c15da2f5353f8d229eb1"
+
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
 } ; 
 
 // firebase.initializeApp(firebaseConfig) ;
